ee9eb05ba6c54d84e839fb9e05f012b94d2422d9
SameepSkillup <sameep.skilluponline@gmail.com>
2025-05-24 14:26:20 -0400

    fullfinal

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,38 +1,33 @@
+// CartSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
     addItem: (state, action) => {
-         const { name, image, cost } = action.payload;
-         const existingItem = state.items.find(item => item.name === name);
-  if (existingItem) {
-    // If item already exists in the cart, increase its quantity
-    existingItem.quantity++;
-  } else {
-    // If item does not exist, add it to the cart with quantity 1
-    state.items.push({ name, image, cost, quantity: 1 });
-  }
+      const item = action.payload;
+      const existingItem = state.items.find(i => i.name === item.name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...item, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
-        
-        state.items = state.items.filter(item => item.name !== action.payload);
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
-        // Find the item in the cart that matches the given name
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-          itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
-        }
-    
-    },
-  },
+      const updatedItem = action.payload;
+      const item = state.items.find(i => i.name === updatedItem.name);
+      if (item) {
+        item.quantity = updatedItem.quantity;
+      }
+    }
+  }
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
